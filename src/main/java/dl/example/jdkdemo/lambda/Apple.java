ac27a699b64c344054a1c45539916a83e3630017
杜磊 <dulei@travelsky.com>
2020-04-22 14:21:56 +0800

    排序

---
+++ src/main/java/dl/example/jdkdemo/lambda/Apple.java
@@ -0,0 +1,18 @@
+package dl.example.jdkdemo.lambda;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * @className Apple
+ * @description TODO
+ * @author DL
+ * @date 2020/1/15 16:28
+ * @version 1.0
+ */
+
+@Data
+@AllArgsConstructor
+public class Apple {
+    private String color;
+}
---
+++ src/main/java/dl/example/jdkdemo/lambda/GetList.java
@@ -0,0 +1,18 @@
+package dl.example.jdkdemo.lambda;
+
+import java.util.List;
+import java.util.function.Function;
+
+
+/**
+ * @className GetList
+ * @description TODO
+ * @author DL
+ * @date 2020/1/15 16:30
+ * @version 1.0
+ */
+
+public interface GetList {
+  List<String>   getColor();
+
+}
